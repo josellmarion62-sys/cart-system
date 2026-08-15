40e52a7034e2d6fb58ff3bcb690c56617b68a8cc
Russell Nunag <russellnunagindependenceict@gmail.com>
2026-08-15 02:00:53 +0000

    Add Cart

---
+++ Add-Russ.py
@@ -0,0 +1,8 @@
+cart = {}
+print ("Add Cart")
+
+item = (input("Add a Item: "))
+
+price = price = float(input("Add Price: ₱"))
+cart [item]=price
+print(f" Added a item {item} just for ₱{price:.2f}")
